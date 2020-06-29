0f951bf4886d12dcf1ad40d1d0683b875a241004
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-06-29 19:12:32 -0300

    chore(FinancialRegisterTable): implements styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ExpensesPage from './components/ExpensesPage'
-import './style/App.css'
+import { FinancialRecordsTable } from './components'
 
 function App() {
   return (
     <div className="App">
-      <ExpensesPage />
+      <FinancialRecordsTable />
     </div>
   );
 }
